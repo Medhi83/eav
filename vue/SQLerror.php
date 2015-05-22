2493d0ef48bc3f7744c375b0fb1d3b1e06c4ceab
Your Name <you@example.com>
2015-05-22 17:59:25 +0200

    Ajout du bouton de deconnexion de la base ensembl et modification du catching des erreurs

---
+++ vue/SQLerror.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>EAV - Erreur</title>
+		<link rel="stylesheet" type="text/css" href="vue/style.css" />
+		<script type="text/javascript" src="vue/jquery/js/jquery.min.js"></script>
+		<style type="text/css"></style>
+	</head>
+
+	<body>
+		<!-- Menu -->
+		<?php include_once ('top_menu.php')?>
+		<!-- Fin du Menu -->
+		
+		<div id="page">
+			<fieldset>
+				<legend class="titre">Erreur</legend>
+				<div class="error_field">
+					<p>Il y a un problème!</p>
+					<p><?php echo $erreur_sql; ?></p>
+				
+				</div>
+			</fieldset>
+		</div>
+		<!-- Pied de page -->
+		<?php include_once ('footer.php')?>
+		<!-- Fin du pied de page -->
+	</body>
+</html>
